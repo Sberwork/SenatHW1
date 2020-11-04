a44366714c29faabd464bf298e08bcfcb2b3389c
HalimHamidov <halimjonik@gmail.com>
2020-11-04 22:20:36 +0300

    feat: add solution , but there is some doubt re owin interconnection

--- SHW1/Middleware/ReqResLogMid.cs
+++ SHW1/Middleware/ReqResLogMid.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Logging;
 
 namespace SHW1.Middleware
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class ReqResLogMid
     {
         private readonly RequestDelegate _next;
@@ -23,26 +22,34 @@ namespace SHW1.Middleware
         public async Task Invoke(HttpContext httpContext)
         {
             //First, get the incoming request
+            // Сначала получаем входящий запрос
             var request = await FormatRequest(httpContext.Request);
 
             //Copy a pointer to the original response body stream
+            //Скопируйте указатель на исходный поток тела ответа
             var originalBodyStream = httpContext.Response.Body;
 
-            //Create a new memory stream...
+            //Create a new memory stream
+            // Создаем новый поток памяти 
             using (var responseBody = new MemoryStream())
             {
-                //...and use that for the temporary response body
+                //and use that for the temporary response body
+                // и используйте это для временного ответа body
                 httpContext.Response.Body = responseBody;
 
                 //Continue down the Middleware pipeline, eventually returning to this class
+                //Продолжайте движение по конвейеру промежуточного ПО, в конечном итоге вернувшись к этому классу
                 await _next(httpContext);
 
                 //Format the response from the server
+                //Отформатируйте ответ от сервера
                 var response = await FormatResponse(httpContext.Response);
 
                 //TODO: Save log to chosen datastore
+                // ЗАДАЧА: Сохранить журнал в выбранное хранилище данных
 
                 //Copy the contents of the new memory stream (which contains the response) to the original stream, which is then returned to the client.
+                // Копируем содержимое нового потока памяти (который содержит ответ) в исходный поток, который затем возвращается клиенту.
                 await responseBody.CopyToAsync(originalBodyStream);
             }
         }
@@ -51,18 +58,23 @@ namespace SHW1.Middleware
             var body = request.Body;
 
             //This line allows us to set the reader for the request back at the beginning of its stream.
+            // Эта строка позволяет нам вернуть читателя для запроса в начало его потока.
             request.EnableBuffering();
 
             //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            // Теперь нам нужно прочитать поток запроса. Сначала мы создаем новый byte [] той же длины, что и поток запроса 
             var buffer = new byte[Convert.ToInt32(request.ContentLength)];
 
-            //...Then we copy the entire request stream into the new buffer.
+            //Then we copy the entire request stream into the new buffer.
+            // Затем мы копируем весь поток запроса в новый буфер.
             await request.Body.ReadAsync(buffer, 0, buffer.Length);
 
-            //We convert the byte[] into a string using UTF8 encoding...
+            //We convert the byte[] into a string using UTF8 encoding
+            // Преобразуем byte [] в строку, используя кодировку UTF8 
             var bodyAsText = Encoding.UTF8.GetString(buffer);
 
             //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            //и, наконец, присваиваем чтение body обратно запроса body , что разрешено из-за EnableRewind ()
             request.Body = body;
 
             return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
@@ -70,16 +82,20 @@ namespace SHW1.Middleware
 
         private async Task<string> FormatResponse(HttpResponse response)
         {
-            //We need to read the response stream from the beginning...
+            //We need to read the response stream from the beginning
+            // Нам нужно прочитать поток ответа с самого начала 
             response.Body.Seek(0, SeekOrigin.Begin);
 
             //...and copy it into a string
+            // и скопируем в строку
             string text = await new StreamReader(response.Body).ReadToEndAsync();
 
             //We need to reset the reader for the response so that the client can read it.
+            // Нам нужно сбросить считыватель ответа, чтобы клиент мог его прочитать.
             response.Body.Seek(0, SeekOrigin.Begin);
 
-            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            //Return the string for the response, including the status code (e.g. 200)
+            // Возвращаем строку ответа, включая код состояния (например, 200)
             return $"{response.StatusCode}: {text}";
         }
     }
--- SHW1/Startup.cs
+++ SHW1/Startup.cs
@@ -1,6 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,18 +25,21 @@ namespace SHW1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Этот метод вызывается средой выполнения. Используйте этот метод для добавления сервисов в контейнер.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Этот метод вызывается средой выполнения. Используйте этот метод для настройки конвейера HTTP-запросов.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             app.UseHttpsRedirection();
 
@@ -46,6 +51,21 @@ namespace SHW1
             {
                 endpoints.MapControllers();
             });
+            app.UseOwin(pipeline =>
+            {
+                pipeline(next => SendResponseAsync);
+            });
         }
-    }
+
+        public Task SendResponseAsync(IDictionary<string, object> environment)
+        {
+            string responseText = "Hello Sber's SENAT Team! Mentors! You are the best of the best! Thank YOU for support!";
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+            var responseStream = (Stream)environment["owin.ResponseBody"];
+
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+}
 }
