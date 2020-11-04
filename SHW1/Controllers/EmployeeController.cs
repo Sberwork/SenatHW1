941a71ef43fce2db10bd574adb8219d18a1013ae
HalimHamidov <halimjonik@gmail.com>
2020-11-04 17:50:06 +0300

    feat: delete second solution

---
+++ SHW1/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SHW1.Models;
+
+namespace SHW1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        /*private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+*/
+        [HttpGet]
+        public ActionResult<Employee> GetById(int id)
+        {
+            var employee = new Employee()
+            {
+                ID = id,
+                FirstName = "firstName",
+                LastName = "lastName",
+                DateofBirth = DateTime.Now.AddYears(-10)
+            };
+
+            return Ok(employee);
+        }
+    }
+}
